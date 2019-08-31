ce35b9b5cd892a177101d5f2e87ebb7af110ddcb
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-08-31 13:18:45 -0400

    add sentence tokenization before feature extraction

--- get_article_embeddings.py
+++ get_article_embeddings.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+from nltk import sent_tokenize
 from universal_sentence_encoder import get_sentence_embeddings
 
 ARTICLES_JSON_PATH = 'data/articles.json'
@@ -10,14 +11,16 @@ with open(ARTICLES_JSON_PATH, 'r') as f:
 
 articles_df = pd.DataFrame(articles)
 articles_df = articles_df.explode('text')
+articles_df['text'] = articles_df['text'].apply(sent_tokenize)
+articles_df = articles_df.explode('text')
 articles_df['text'] = articles_df['text'].str.strip()
 
 # Duplicated text across will be assumed to be boilerplate
-# 3 was based on limited inspection and pretty arbitrary
+# 100 is pretty arbitrary (300 total articles when chosen)
 article_text_df = articles_df.groupby(['text']).size().reset_index(name='count')
-article_text_df = article_text_df.loc[article_text_df['count'] < 3]
+article_text_df = article_text_df.loc[article_text_df['count'] < 100]
 
-article_embeddings = get_sentence_embeddings(article_text_df['text'].tolist(), chunk_size=256)  # base 2 to look smart
+article_embeddings = get_sentence_embeddings(article_text_df['text'].tolist(), chunk_size=512)  # base 2 to look smart
 article_embeddings_df = pd.DataFrame(article_embeddings)
 article_embeddings_df['text'] = article_text_df['text']
 article_embeddings_df.to_csv(EMBEDDINGS_CSV_PATH, index=False)
