a9ba9413426684040e5f4da8ac62a5e7c671ff4e
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-08-31 10:56:41 -0400

    run larger scrape

--- scrape_nasdaq.py
+++ scrape_nasdaq.py
@@ -8,7 +8,7 @@ OVERWRITE = False
 
 if RE_SCRAPE or not os.path.exists(ARTICLES_JSON_PATH):
     scraper = NasdaqScraper()
-    scraper.scrape_articles(n_pages=1, n_tries=3)
+    scraper.scrape_articles(n_pages=10, n_tries=3)
     scraper.json_dump_articles(ARTICLES_JSON_PATH, extend=not OVERWRITE)
 
 with open(ARTICLES_JSON_PATH, 'r') as f:
